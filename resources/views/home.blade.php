af2831efdc22b6c41cdb1a2bcc1ec672912199f2
Mary Soco Ortiz <marysoco.ortiz@gmail.com>
2019-06-25 17:39:15 -0500

    Updated content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
 <slider class="main-slider">
     @foreach($sliders->sortBy('order') as $slider)
     <slide>
-        <div class="slide h-screen bg-cover bg-no-repeat bg-center" style="background-image:url({{ asset($slider->url) }})">
+        <div class="multiply h-screen bg-cover bg-no-repeat bg-center" style="background-image:url({{ asset($slider->url) }})">
             <div class="container py-32 text-white">
                 @markdown( $slider->text )
             </div>
@@ -33,7 +33,7 @@
             <div class="w-full md:w-7/12 ml-auto">
                 <slider class="shadow-2xl">
                     <slide>
-                        <div class="py-56 px-20  bg-cover bg-no-repeat bg-center relative" style="background-image:url({{ asset('images/slider/slider-01.jpg') }})">
+                        <div class="multiply py-56 px-20  bg-cover bg-no-repeat bg-center relative" style="background-image:url({{ asset('images/mi_jalisco_home.jpg') }})">
                             <div class="text-white absolute bottom-16">
                                 <h2>Tapalpa</h2>
                                 <p>La paz e inspiración del bosque</p>
@@ -48,34 +48,34 @@
 
 <section class="categories font-secondary leading-tight">
     <div class="flex flex-wrap text-white text-center font-bold">
-        <div class="category w-1/2 lg:w-1/5 bg-red" style="background-image:url({{ asset('images/categories/experiencias.jpg') }})">
+        <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_titulo.jpg') }})">
             <h2 class="text-2xl">Experiencias</h2>
         </div>
-        <div class="category w-1/2 lg:w-1/5 bg-gray" style="background-image:url({{ asset('images/categories/cultura.jpg') }})">
+        <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_cultura.jpg') }})">
             <h2 class="text-2xl">Cultura</h2>
         </div>
-         <div class="category w-1/2 lg:w-1/5 " style="background-image:url({{ asset('images/categories/deportivo.jpg') }})">
+         <div class="category w-1/2 lg:w-1/5 " style="background-image:url({{ asset('images/categories/experiensa_home_deporte.jpg') }})">
             <h2 class="text-2xl">Deportivo</h2>
         </div>
-        <div class="category w-1/2 lg:w-1/5 bg-orange">
+        <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_gastronomia.jpg') }})">
             <h2 class="text-2xl">Gastronómico</h2>
         </div>
-        <div class="category w-1/2 lg:w-1/5 bg-gray" style="background-image:url({{ asset('images/categories/lgbt.jpg') }})">
+        <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_lgbt.jpg') }})">
             <h2 class="text-2xl">LGBTTTIQ</h2>
         </div>
-        <div class="category w-1/2 lg:w-1/5 bg-gray" style="background-image:url({{ asset('images/categories/naturaleza.jpg') }})">
+        <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_naturaleza.jpg') }})">
             <h2 class="text-2xl">Naturaleza</h2>
         </div>
-        <div class="category w-1/2 lg:w-1/5 bg-orange">
+        <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_negocios.jpg') }})">
             <h2 class="text-2xl">Negocio y reuniones</h2>
         </div>
-         <div class="category w-1/2 lg:w-1/5 bg-gray" style="background-image:url({{ asset('images/categories/religioso.jpg') }})">
+         <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_religioso.jpg') }})">
             <h2 class="text-2xl">Religioso</h2>
         </div>
-        <div class="category w-1/2 lg:w-1/5 bg-gray" style="background-image:url({{ asset('images/categories/salud.jpg') }})">
+        <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_salud.jpg') }})">
             <h2 class="text-2xl">Salud y Bienestar</h2>
         </div>
-        <div class="category w-1/2 lg:w-1/5 bg-purple ">
+        <div class="category w-1/2 lg:w-1/5" style="background-image:url({{ asset('images/categories/experiensa_home_playa.jpg') }})">
             <h2 class="text-2xl">Sol y Playa</h2>
         </div>
     </div>
@@ -85,32 +85,32 @@
     <div class="container py-16">
         <div class="flex flex-wrap mb-8">
             <div class="w-1/2 bg-white">
-                <div class="bg-cover bg-no-repeat bg-center p-20 -mt-4 -ml-4 shadow-md" style="background-image:url({{ asset('images/events/coordenada.jpg') }})"></div>
-            </div>
-            <div class="w-1/2 bg-white p-8">
-                <h2>Coordenada 2019</h2>
-                <p><font-awesome-icon :icon="['fas', 'map-marker-alt']" fixed-width class="text-red mr-1"></font-awesome-icon>Parque Trasloma. GDL</p>
-                <p>8 y 19 de Octubre, 2019</p>
-            </div>
-        </div>
-        <div class="flex flex-wrap mb-8">
-            <div class="w-1/2 bg-white">
-                <div class="bg-cover bg-no-repeat bg-center p-20 -mt-4 -ml-4 shadow-md" style="background-image:url({{ asset('images/events/gdlluz.jpg') }})"></div>
-            </div>
-            <div class="w-1/2 bg-white p-8">
-                <h2>GDLUZ</h2>
-                <p><font-awesome-icon :icon="['fas', 'map-marker-alt']" fixed-width class="text-red mr-1"></font-awesome-icon>Parque Trasloma. GDL</p>
-                <p>8 y 19 de Octubre, 2019</p>
-            </div>
-        </div>
-        <div class="flex flex-wrap mb-8">
-            <div class="w-1/2 bg-white">
-                <div class="bg-cover bg-no-repeat bg-center p-20 -mt-4 -ml-4 shadow-md" style="background-image:url({{ asset('images/events/openvallarta.jpg') }})"></div>
+                <div class="bg-cover bg-no-repeat bg-center p-20 -mt-4 -ml-4 shadow-md" style="background-image:url({{ asset('images/events/eventos_home_open_vallarta.jpg') }})"></div>
             </div>
             <div class="w-1/2 bg-white p-8">
                 <h2>Open Vallarta</h2>
-                <p><font-awesome-icon :icon="['fas', 'map-marker-alt']" fixed-width class="text-red mr-1"></font-awesome-icon>Parque Trasloma. GDL</p>
-                <p>8 y 19 de Octubre, 2019</p>
+                <p><font-awesome-icon :icon="['fas', 'map-marker-alt']" fixed-width class="text-red mr-1"></font-awesome-icon>Puerto Vallarta</p>
+                <p>29 de abril - 5 de mayo</p>
+            </div>
+        </div>
+        <div class="flex flex-wrap mb-8">
+            <div class="w-1/2 bg-white">
+                <div class="bg-cover bg-no-repeat bg-center p-20 -mt-4 -ml-4 shadow-md" style="background-image:url({{ asset('images/events/eventos_home_mariachi.jpg') }})"></div>
+            </div>
+            <div class="w-1/2 bg-white p-8">
+                <h2>Encuentro Internacional de Mariachi y Charrería 2019</h2>
+                <p><font-awesome-icon :icon="['fas', 'map-marker-alt']" fixed-width class="text-red mr-1"></font-awesome-icon>Guadalajara</p>
+                <p>23 de agosto - 1 de septiembre</p>
+            </div>
+        </div>
+        <div class="flex flex-wrap mb-8">
+            <div class="w-1/2 bg-white">
+                <div class="bg-cover bg-no-repeat bg-center p-20 -mt-4 -ml-4 shadow-md" style="background-image:url({{ asset('images/events/eventos_home_coordenada.jpg') }})"></div>
+            </div>
+            <div class="w-1/2 bg-white p-8">
+                <h2>Coordenada</h2>
+                <p><font-awesome-icon :icon="['fas', 'map-marker-alt']" fixed-width class="text-red mr-1"></font-awesome-icon>Guadalajara</p>
+                <p>18 - 19 de octubre</p>
             </div>
         </div>
         <div class="flex w-full">
@@ -122,10 +122,14 @@
 <section class="routes bg-cover bg-no-repeat bg-center" style="background-image:url({{ asset('images/bg-routes.jpg') }})">
     <div class="container py-10 px-10 lg:px-0">
         <div class="w-full bg-white p-10 rounded-lg">
-            <img class="w-full" src="{{ asset('images/routes/ruta-historica.jpg') }}">
+            <img class="w-full" src="{{ asset('images/routes/ruta_banner_fransiscana.jpg') }}">
             <div class="px-4 text-sm">
-                <h2 class="font-secondary font-bold text-3xl my-2">Ruta de las catedrales</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac massa pellentesque tortor tristique varius. Praesent eu felis sit amet magna pellentesque rhoncus. Nulla sem mauris, posuere in dapibus vel, pretium aliquam ligula. Nunc fermentum ligula sed quam molestie, tempor suscipit lectus sodales. Integer sit amet risus nibh. Proin ac metus nisi. Integer aliquet quam vitae mi volutpat, in convallis orci euismod. Cras aliquet sem a mollis pretium.</p>
+                <h2 class="font-secondary font-bold text-3xl my-2">Ruta Franciscana de Tlajomulco</h2>
+                <p>Esta ruta recorre un total de 10 templos donde podrás conocer parte
+                    de la historia de Tlajomulco, 
+                    uno de los municipios que conforman la Zona Metropolitana de
+                    Guadalajara ubicándose al sur de la perla tapatía. 
+                    ¡Descúbrelos!</p>
             </div>
         </div>
 
@@ -133,20 +137,20 @@
             <div class="flex flex-wrap -mx-2">
                 <div class="w-full lg:w-1/3 px-2 py-8 lg:py-0">
                     <div class="bg-white p-4 rounded-lg text-center">
-                        <div class="bg-cover bg-no-repeat bg-center p-32 mb-6" style="background-image:url({{ asset('images/routes/ruta-historica.jpg') }})"></div>
-                        <h2 class="font-secondary font-bold text-3xl my-2 leading-none">Ruta de los bosques</h2>
+                        <div class="bg-cover bg-no-repeat bg-center p-32 mb-6" style="background-image:url({{ asset('images/routes/ruta_banner_costa_alegre.jpg') }})"></div>
+                        <h2 class="font-secondary font-bold text-3xl my-2 leading-none">Ruta Costalegre</h2>
                     </div>
                 </div>
                 <div class="w-full lg:w-1/3 px-2 py-8 lg:py-0">
                     <div class="bg-white p-4 rounded-lg text-center">
-                        <div class="bg-cover bg-no-repeat bg-center p-32 mb-6" style="background-image:url({{ asset('images/routes/ruta-historica.jpg') }})"></div>
-                        <h2 class="font-secondary font-bold text-3xl my-2 leading-none">Ruta histórica</h2>
+                        <div class="bg-cover bg-no-repeat bg-center p-32 mb-6" style="background-image:url({{ asset('images/routes/ruta_banner_huachimontones.jpg') }})"></div>
+                        <h2 class="font-secondary font-bold text-3xl my-2 leading-none">Ruta Guachimontones</h2>
                     </div>
                 </div>
                 <div class="w-full lg:w-1/3 px-2 py-8 lg:py-0">
                     <div class="bg-white p-4 rounded-lg text-center">
-                        <div class="bg-cover bg-no-repeat bg-center p-32 mb-6" style="background-image:url({{ asset('images/routes/ruta-historica.jpg') }})"></div>
-                        <h2 class="font-secondary font-bold text-3xl my-2 leading-none">Ruta del arte</h2>
+                        <div class="bg-cover bg-no-repeat bg-center p-32 mb-6" style="background-image:url({{ asset('images/routes/ruta_banner_agave.jpg') }})"></div>
+                        <h2 class="font-secondary font-bold text-3xl my-2 leading-none">Ruta Paisaje Agavero</h2>
                     </div>
                 </div>
             </div>
