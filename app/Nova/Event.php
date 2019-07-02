8f1dcc354c8e50554543a49416c432ac6f080437
Mary Soco Ortiz <marysoco.ortiz@gmail.com>
2019-07-01 20:00:30 -0500

    Added events table

---
+++ app/Nova/Event.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Fields\Image;
+use Laravel\Nova\Fields\Markdown;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Select;
+
+class Event extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Event';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->onlyOnForms(),
+            Text::make('Nombre', 'name')
+                ->sortable(),
+            Text::make('Fecha', 'date')
+                ->sortable(),
+            Text::make('Lugar', 'place')
+                ->sortable(),
+            Markdown::make('Texto', 'text')
+                ->rules('nullable')
+                ->hideFromIndex(),
+            Text::make('Enlace', 'url')
+                ->hideFromIndex()
+                ->sortable(),
+            Select::make('Target')->options([
+                '_blank' => 'Blank',
+                '_self' => 'Self',
+            ])->hideFromIndex(),
+            Image::make('Imagen', 'image')
+                ->disk('public')
+                ->path('slider')
+                ->rules('max:1024')
+                ->creationRules('required')
+                ->updateRules('nullable')
+                ->prunable()
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
