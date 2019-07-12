d9d9ff34abed52f2243a0ba9960951e74aac9d9a
Mary Soco Ortiz <marysoco.ortiz@gmail.com>
2019-07-11 20:41:47 -0500

    Added translation strings for Jalisco page (blue section)

--- resources/lang/es/jalisco.php
+++ resources/lang/es/jalisco.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    
+    'foundation' => 'Fundación',
+    'coordinates' => 'Coordenadas',
+    'capital' => 'Capital',
+    'entity' => 'Entidad',
+    'state' => 'Estado',
+    'surface' => 'Superficie',
+    'total' => 'Total',
+    'altitude' => 'Altitud',
+    'media' => 'Media',
+    'maximum' => 'Máxima',
+    'population' => 'Población',
+    'density' => 'Densidad',
+    'demonym' => 'Gentilicio',
 ];
--- resources/views/jalisco.blade.php
+++ resources/views/jalisco.blade.php
@@ -10,8 +10,6 @@
 <section class="headerimage multiply-40 h-screen bg-cover bg-no-repeat bg-center" style="background-image:url({{ asset('images/bg-jalisco.jpg') }})">
     <div class="container text-content flex w-full font-secondary h-full relative">
         <div class="flex-1 text-left text-white absolute bottom-0 pb-8 text-2xl px-10 lg:px-0">
-            {{--<h1 class="mx-auto text-4xl lg:text-6xl font-normal">Esto es <span class="font-bold">Jalisco</span></h1>--}}
-            {{--<p>La palabra Jalisco, formada de las palabras náhuatl xalli, que significa arena, ixtli, que significa superficie y co, desinencia de lugar, significa “sobre el arenal” y sobre este, se han levantado siglos de historia, tradición, folclor, cultura, arte y mucho más.</p>--}}
             @markdown($jalisco->header_text)
         </div>
     </div>
@@ -21,8 +19,6 @@
     <div class="container py-12">
         <div class="flex flex-wrap">
             <div class="w-full lg:w-5/12 text-2xl text-gray-500 px-10 lg:px-0">
-                {{--<p>Se especula que el territorio de Jalisco, antes de ser Jalisco, ha sido habitado desde hace 15,000 años. Se han encontrado vestigios de culturas Toltecas, primero y Chichimecas después. En 1530, tras la llegada de los españoles, se fundó la Guadalajara, que sería  capital del Reino de Nueva Galicia.  Ésta fue la ciudad en la que Miguel Hidalgo y Costilla promulgaría la abolición de la esclavitud durante la Guerra de Independencia.</p>
-                <p>Fue hasta el 16 de junio de 1823, tras la caída del Imperio Mexicano, que se fundara el Estado Libre y Soberano de Xalisco (antes el sonido de la “J” se representaba con una “X”, como sigue ocurriendo en el nombre de “México”), manteniendo a Guadalajara como su capital y dando origen a una entidad que daría mucho de qué hablar.</p>--}}
                 @markdown($jalisco->history)
             </div>
             <div class="w-full lg:w-6/12 ml-auto px-10 lg:px-0">
@@ -65,28 +61,28 @@
         <div class="flex flex-wrap w-full text-white font-semibold text-xl">
            <div class="w-full lg:w-1/3 text-center ">
                 <img class="w-1/2 mx-auto mb-4" src="{{ asset('images/logo-estado.svg') }}">
-                <p>Fundación</p>
+                <p>@lang('jalisco.foundation')</p>
                 <p>16 de junio de 1823 (195 años)</p>
             </div>
             <div class="w-full lg:w-2/3 flex flex-wrap px-10 lg:px-0">
                 <div class="w-full my-4">
-                    <p>Coordenadas: 20°34′00″N 103°40′35″O</br>
-                    Capital: Guadalajara</br>
-                    Entidad: Estado</p>
+                    <p>@lang('jalisco.coordinates'): 20°34′00″N 103°40′35″O</br>
+                    @lang('jalisco.capital'): Guadalajara</br>
+                    @lang('jalisco.entity'): @lang('jalisco.state')</p>
                 </div>
                 <div class="w-full lg:w-1/2 my-4">
-                    <p>Superficie: Puesto 7.º de 32</br>
-                        Total: 78,588 km²</br>
-                        Altitud	 </br>
-                        • Media	1355 m s. n. m.</br>
-                        • Máxima 4 260 m s. n. m.</p>
+                    <p>@lang('jalisco.surface'): Puesto 7.º de 32</br>
+                        @lang('jalisco.total'): 78,588 km²</br>
+                        @lang('jalisco.altitude')	 </br>
+                        • @lang('jalisco.media')	1355 m s. n. m.</br>
+                        • @lang('jalisco.maximum') 4 260 m s. n. m.</p>
                 </div>
                 <div class="w-full lg:w-1/2 my-4">
-                    <p>Población </br>
+                    <p>@lang('jalisco.population') </br>
                     Puesto 4.º de 32</br>
-                    • Total :7 844 830 hab.2​</br>
-                    • Densidad : 102,1 hab/km² (13)</br>
-                    Gentilicio	Jalisciense</p>
+                    • @lang('jalisco.total') :7 844 830 hab.2​</br>
+                    • @lang('jalisco.density') : 102,1 hab/km² (13)</br>
+                    @lang('jalisco.demonym')	Jalisciense</p>
                 </div>
             </div>
        </div>
@@ -97,16 +93,9 @@
     <div class="container py-12">
         <div class="flex flex-wrap w-full text-2xl text-gray-500 px-10 lg:px-0">
             <div class="w-full">
-                {{--<h1 class="font-normal text-6xl leading-tight font-secondary mb-8">¡Mucho gusto, </br>
-                    <span class="font-bold">Jalisco!</span></h1>
-                <p>Jalisco está conformado por 125 municipios, ocho de los cuales cuentan con el distintivo de Pueblo Mágico. Posiblemente has escuchado hablar de Zapopan, Puerto Vallarta o Tlaquepaque, y cada uno de ellos cuenta con algún atractivo especial, como son Tapalpa o Mascota, y ya sea que pruebes las guasanas de Mazamitla, o admires los paisajes de Agave Azul en Tequila, aquí se viven experiencias que en ningún otro lado se viven igual.</p>
-                <p>Hoy en día Jalisco se distingue por la gran diversidad que envuelve a su geografía.  Tanto en términos de clima, pudiendo pasar de la cálida playa al fresco bosque en unas horas de camino, como en la posibilidad visitar una ciudad a la altura de las grandes urbes, o perderse en callejones llenos de artesanías y tradición, todo a una distancia mínima. </p>--}}
                 @markdown($jalisco->info_top)
             </div>
             <div class="w-full lg:w-1/2">
-                {{--<p>Así mismo, Jalisco es un estado representativo en diferentes ámbitos: económicamente realiza grandes aportes por su producción de distintos productos agrícolas, al mismo tiempo que es sede de diferentes empresas de tecnología y cuenta con excelentes espacios para foros y convenciones; en términos culturales, varios de los elementos icónicos de México, como lo son el tequila o el mariachi, tienen su hogar y origen en nuestras latitudes. </p>
-                <p>La cocina del estado cuenta con distintivos como el birote salado, el queso adobera o la carne en su jugo y el entretenimiento y la diversión no se quedan atrás gracias a los múltiples foros y espacios dedicados al esparcimiento con los que contamos.</p>
-                <p>Jalisco es un estado lleno de los más agradables contrastes, diverso en su gente y abierto a todas las personas que lo quieran visitar. Descubre por tu propia cuenta dónde está la magia de Jalisco, visítalo y hazlo tuyo.   </p>--}}
                 @markdown($jalisco->info_left)
             </div>
             <div class="w-full lg:w-5/12 ml-auto">
