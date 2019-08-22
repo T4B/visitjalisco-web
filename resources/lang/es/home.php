1a1127f5b7f23381f065ccdb317d8a47bf4cec4d
Fernando Gutierrez <fernando@beeb.mx>
2019-08-22 12:50:46 -0500

    Update home lang (es)

--- resources/lang/es/home.php
+++ resources/lang/es/home.php
@@ -3,8 +3,8 @@
 return [
     'more-events' => 'Más eventos',
     'more-routes' => 'Más rutas',
-    'mi-jalisco-title'  => 'Este es *Mi Jalisco*',
-    'mi-jalisco-text'  => 'Estas son las historias y </br>experiencias que nuestras </br>regiones ofrecen, </br><span class="font-bold">Vividas a profundidad.</span>',
+    'mi-jalisco-title' => 'Este es *Mi Jalisco*',
+    'mi-jalisco-text' => 'Estas son las historias y </br>experiencias que nuestras </br>regiones ofrecen, </br><span class="font-bold">vividas a profundidad.</span>',
     'this-is' => 'Este es',
     'mi-jalisco' => 'Mi Jalisco',
 ];
